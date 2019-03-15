d2625dd324af9f342511e6041c199c25370e46c6
Camila L <camila.leite.oliveira@gmail.com>
2019-03-15 17:59:47 -0300

    Create footer.php

---
+++ footer-admin.php
@@ -0,0 +1,23 @@
+		</div>
+	</div>
+</section>
+<footer class="footer">
+	<div class="container">
+		<?php echo $Load->FooterMenu(); ?>
+		<div class="content has-text-centered">
+			<div class="columns is-mobile is-centered">
+				<div class="field is-grouped is-grouped-multiline"><?php echo FOOTER; ?></div>
+			</div>
+		</div>
+	</div>
+</footer>
+<!-- plugins:js -->
+<script async type="text/javascript" src="assets/js/bulma.js"></script>
+<!-- endinject -->
+<!-- Plugin js for all those pages-->
+<script src="assets/node_modules/dropify/dist/js/dropify.min.js"></script>
+<!-- End plugin js for all those page-->
+<!-- Custom js for all pages-->
+<script src="assets/js/dropify.js"></script>
+<script src="assets/js/menu.js"></script>
+<!-- End custom js for all pages-->
---
+++ footer-index.php
@@ -0,0 +1,8 @@
+		</div>
+		<div class="hero-foot">
+			<div class="container">
+				<div class="content has-text-centered"><?php echo FOOTER; ?></div>
+			</div>
+		</div>
+	</div>
+</section>
