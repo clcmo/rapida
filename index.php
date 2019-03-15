b5a3e1c24a10f7cf6b2e052931433d77550486b2
Camila L <camila.leite.oliveira@gmail.com>
2019-03-15 17:59:57 -0300

    Create index.php

---
+++ index.php
@@ -0,0 +1,29 @@
+<?php
+    /**
+     * The landing page for our theme.
+     *
+     * Displays all of the brand menu content and index
+     *
+     * @package Bulma by Milla
+     */
+    include('header-index.php');
+?>
+        <div class="hero-body">
+            <div class="container has-text-centered">
+                <div class="column is-6 is-offset-3">
+                    <h1 class="title is-logo is-large">Estamos Chegando</h1>
+                    <h2 class="subtitle">A Rápida está em desenvolvimento. Para saber planos e pacotes, insira seu e-mail e saiba antes de todo mundo.</h2>
+                    <form action="" method="post">
+                        <div class="box">
+                            <div class="field is-grouped">
+                                <p class="control is-expanded"><input class="input" type="text" placeholder="Insira seu e-mail"></p>
+                                <p class="control"><input class="button is-info" type="submit" name="send" value="Me Informe" /></p>
+                            </div>
+                        </div>
+                    </form>
+                </div>
+            </div>
+        </div>
+<?php 
+
+    include('footer-index.php'); ?>
