557369787466220bc2caeccf5480d022255f9a49
Camila L <camila.leite.oliveira@gmail.com>
2019-03-15 19:23:26 -0300

    Update header-admin.php

--- header-admin.php
+++ header-admin.php
@@ -1,13 +1,13 @@
 <?php
-/**
- * The header for our admin pages.
- *
- * Displays all of the brand menu and user section
- *
- * @package Milla Meets Bulma
- */
-include('header.php');
-session_start();
+	/**
+	 * The header for our admin pages.
+	 *
+	 * Displays all of the brand menu and user section
+	 *
+	 * @package Bulma by Milla
+	 */
+	include('header.php');
+	session_start();
 ?>
 <section class="hero is-info is-medium is-white">
     <div class="container has-text-centered">
