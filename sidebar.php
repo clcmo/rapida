71a9b4e8774c885f327ddd49442c1427386ddf0d
Camila L <camila.leite.oliveira@gmail.com>
2019-03-15 19:25:27 -0300

    Create sidebar.php

---
+++ sidebar.php
@@ -0,0 +1,5 @@
+<section class="section">    
+    <div class="container">
+        <!--<div class="columns">
+            <div class="column is-2"><?php echo $User->ShowSidebarMenu($_SESSION['id']); ?></div>
+            <div class="column is-10">-->
