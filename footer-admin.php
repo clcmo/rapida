62450733b07bced12ac4557b413389b05e004cb1
Camila L <camila.leite.oliveira@gmail.com>
2019-03-15 18:34:29 -0300

    Update footer-admin.php

--- footer-admin.php
+++ footer-admin.php
@@ -12,12 +12,12 @@
 	</div>
 </footer>
 <!-- plugins:js -->
-<script async type="text/javascript" src="assets/js/bulma.js"></script>
+<!--<script async type="text/javascript" src="assets/js/bulma.js"></script>-->
 <!-- endinject -->
 <!-- Plugin js for all those pages-->
-<script src="assets/node_modules/dropify/dist/js/dropify.min.js"></script>
+<!--<script src="assets/node_modules/dropify/dist/js/dropify.min.js"></script>-->
 <!-- End plugin js for all those page-->
 <!-- Custom js for all pages-->
-<script src="assets/js/dropify.js"></script>
-<script src="assets/js/menu.js"></script>
+<!--<script src="assets/js/dropify.js"></script>-->
+<!--<script src="assets/js/menu.js"></script>-->
 <!-- End custom js for all pages-->
