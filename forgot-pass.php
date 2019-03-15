8216950753fb728b65375837b36c506ccc4639a6
Camila L <camila.leite.oliveira@gmail.com>
2019-03-15 19:30:46 -0300

    Update forgot-pass.php

--- forgot-pass.php
+++ forgot-pass.php
@@ -25,33 +25,57 @@
       <a href="help">Ajuda</a>
     </p>
     <p class="subtitle is-6">
-    <?php
-      if(isset($_POST['recover'])) {
-        // Resgata variáveis do formulário
-        $email = isset($_POST['email']) ? $_POST['email'] : '';
+      <?php
+        if(isset($_POST['recover'])) {
+          // Resgata variáveis do formulário
+          $email = isset($_POST['email']) ? $_POST['email'] : '';
 
-        //Verifica se os campos estão vazios e exibe uma mensagem de erro
-        if (empty($email)) {
-          echo 'Informe email.';
-          exit;
-        }
+          //Verifica se os campos estão vazios e exibe uma mensagem de erro
+          if (empty($email)) {
+            echo 'Informe email.';
+            exit;
+          }
 
-        //verifica se o usuário existe e exibe ou uma mensagem de erro ou vai ao cadastro
-        $sql = $Tables->LoadFrom('users WHERE email = '.$email.' AND status_use = 1 LIMIT 1');
-        $con = $PDO->prepare($sql) or die ($PDO);
-        if(count($con) == 1){
-            $password = $Load->RandomPass();
-            $password = $Tables->HashStr($password);
+          //verifica se o usuário existe e exibe ou uma mensagem de erro ou vai ao cadastro
+          $sql = $Tables->LoadFrom('users WHERE email = '.$email.' AND status_use = 1 LIMIT 1');
+          $con = $PDO->prepare($sql) or die ($PDO);
+          if(count($con) == 1){
+              $password = $Load->RandomPass();
+              $password = $Tables->HashStr($password);
 
-            $sql = $Tables->LoadFrom('users WHERE email = '.$email.' AND password = :password AND status_use = 1 LIMIT 1');
-            $stmt = $PDO->prepare($sql);
-            $stmt->bindParam(':password', $password);
-            $stmt->execute();
-            $users = $stmt->fetchAll(PDO::FETCH_ASSOC);
-            if (count($users) <= 0) {
-              echo 'Um erro aconteceu';
-              exit;
-            }
+              $sql = $Tables->LoadFrom('users WHERE email = '.$email.' AND password = :password AND status_use = 1 LIMIT 1');
+              $stmt = $PDO->prepare($sql);
+              $stmt->bindParam(':password', $password);
+              $stmt->execute();
+              $users = $stmt->fetchAll(PDO::FETCH_ASSOC);
+              if (count($users) <= 0) {
+                echo 'Um erro aconteceu';
+                exit;
+              }
+
+            //Busca os resultados e os cataloga com a variável $_SESSION
+            $user = $users[0];
+            //session_start();
+            $_SESSION['logged_in'] = true;
+            
+            $_SESSION['id'] = $user['id_use'];
+            $_SESSION['name'] = $user['name_use'];
+            header('Location: admin.php');
+              //$password = $Tables->HashStr();
+              //echo 'Sua nova senha é '.$password;
+              //echo 'Sua nova senha será encaminhada por email';
+          }
+          
+         /* //Verificar se o usuário existe e se a senha é a mesma     
+          $stmt = $PDO->prepare($sql);
+          $stmt->bindParam(':email', $email);
+          $stmt->execute();
+          $users = $stmt->fetchAll(PDO::FETCH_ASSOC);
+
+          if (count($users) <= 0) {
+            echo 'Email ou senha incorretos.';
+            exit;
+          }
 
           //Busca os resultados e os cataloga com a variável $_SESSION
           $user = $users[0];
@@ -60,36 +84,12 @@
           
           $_SESSION['id'] = $user['id_use'];
           $_SESSION['name'] = $user['name_use'];
-          header('Location: admin.php');
-            //$password = $Tables->HashStr();
-            //echo 'Sua nova senha é '.$password;
-            //echo 'Sua nova senha será encaminhada por email';
+          //$_SESSION['type'] = $user['tipo_usu'];
+          //$_SESSION['year_date'] = date('Y', strtotime($user['cadastro']));
+
+          header('Location: admin.php');*/
         }
-        
-       /* //Verificar se o usuário existe e se a senha é a mesma     
-        $stmt = $PDO->prepare($sql);
-        $stmt->bindParam(':email', $email);
-        $stmt->execute();
-        $users = $stmt->fetchAll(PDO::FETCH_ASSOC);
-
-        if (count($users) <= 0) {
-          echo 'Email ou senha incorretos.';
-          exit;
-        }
-
-        //Busca os resultados e os cataloga com a variável $_SESSION
-        $user = $users[0];
-        //session_start();
-        $_SESSION['logged_in'] = true;
-        
-        $_SESSION['id'] = $user['id_use'];
-        $_SESSION['name'] = $user['name_use'];
-        //$_SESSION['type'] = $user['tipo_usu'];
-        //$_SESSION['year_date'] = date('Y', strtotime($user['cadastro']));
-
-        header('Location: admin.php');*/
-      }
-    echo '
+      ?>
     </p>
-  </div>';
-  include('footer-index.php');
+  </div>    
+<?php include('footer-index.php'); ?>
