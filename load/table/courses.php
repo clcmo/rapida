be2e932a90de7ac7ac2676b736bfa0a6e4f1e7a2
Camila L <camila.leite.oliveira@gmail.com>
2019-03-22 19:54:59 -0300

    Update courses.php

--- load/table/courses.php
+++ load/table/courses.php
@@ -3,20 +3,27 @@
 	$sql = $Tables->LoadFrom($script);
 	$query = $PDO->query($sql) or die ($PDO);
 	while($row = $query->fetch(PDO::FETCH_OBJ)){
-		switch ($row->status_cou) {
-			case 1:
-				# desativar...
-				$button_title = '<a href="'.SERVER.'deactivate?id='.$row->id_cou.'" class="button is-danger is-small"><i class="fas fa-minus-circle"></i></a>';
-			break;
-			
-			case 2:
-				# ativar...
-				$button_title = '<a href="'.SERVER.'activate?id='.$row->id_cou.'" class="button is-success is-small"><i class="fas fa-check-square"></a>';
-			break;
-		}
 		$col_1 = '<a href="'.SERVER.'?id='.$row->id_cou.'" class="button is-link is-small"><i class="fas fa-pencil-alt"></i></a>';
 
-		$checked1 = ($row->type_cou = 1) ? 'checked' : '';
-		$checked2 = ($checked1 == true) ? '' : 'checked';
+		//$type_table = $Tables->Found_Item('type', $name_page);
+		switch ($row->type_cou) {
+			case 1: 
+				$button_title_2 = 'Ensino Médio';
+				$checked1 = 'checked';
+				$checked2 = '';
+			break;
+			case 2: 
+				$button_title_2 = 'Ensino Modular';
+				$checked2 = 'checked';
+				$checked1 = '';
+			break;
+		}
+
+		$link = ($row->status_cou) ? 'deactivate' : 'activate';
+		$color = ($row->status_cou) ? 'danger' : 'success' ;
+		$icon = ($row->status_cou) ? '<i class="fas fa-minus-circle"></i>' : '<i class="fas fa-check-square">';
+		$col_4 = '<a href="'.SERVER.''.$link.'?t='.$name_page.'?id='.$row->id_cou.'" class="button is-small is-'.$color.'">'.$icon.'</a>';
+		$button_title = 'Editar';
 	}
+	$titulo = 'Cursos';
 	include('main.php');
