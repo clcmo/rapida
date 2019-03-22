7a4e9dbd4d93131dd6b4663e6086647b31a73bb3
Camila L <camila.leite.oliveira@gmail.com>
2019-03-22 19:55:01 -0300

    Update classroom.php

--- load/table/classroom.php
+++ load/table/classroom.php
@@ -3,4 +3,19 @@
     $script = $name_page.', courses WHERE '.$name_page.'.id_cou = courses.id_cou';
     $button_title = 'Ver Alunos';
     $type_table = $Tables->Found_Item('type', 'courses');
+    $titulo = 'Turmas';
+
+    $sql = $Tables->LoadFrom($script);
+    $query = $PDO->query($sql) or die ($PDO);
+    while($row = $query->fetch(PDO::FETCH_OBJ)){
+		//$type_table = $Tables->Found_Item('type', $name_page);
+		switch ($row->type_cou) {
+			case 1: 
+				$button_title_2 = 'Ensino Médio'; 
+			break;
+			case 2: 
+				$button_title_2 = 'Ensino Modular'; 
+			break;
+		}
+	}
     include('main.php');
