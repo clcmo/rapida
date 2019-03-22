952c8abc3a5c1916e5fc81046972209c0a80cfb1
Camila L <camila.leite.oliveira@gmail.com>
2019-03-22 19:54:56 -0300

    Update main.php

--- load/table/main.php
+++ load/table/main.php
@@ -3,7 +3,7 @@
 
 	$sql = $Tables->LoadFrom($script);
     $query = $PDO->query($sql) or die ($PDO);
-    $titulo = ($name_page != 'notifies') ? 'Turmas' : 'Notificações';
+    //$titulo = ($name_page != 'notifies') ? 'Turmas' : 'Notificações';
 
     echo '
 		<div class="card events-card">
@@ -18,44 +18,12 @@
 					    while($row = $query->fetch(PDO::FETCH_OBJ)){
 							$name_table = $Tables->Found_Item('name', $script);
 							$id = $Tables->Found_Item('id', $script);
-							
-							switch ($row->$type_table) {
-								case 1:
-									//$button_title_2 = ($name_page != 'notifies') ? 'Ensino Médio' : 'Solicitação';
-								break;
-
-								case 2:
-									//$button_title_2 = ($name_page != 'notifies') ? 'Ensino Modular' : 'Revisão';
-								break;
-
-								case 3:
-									//$button_title_2 = ($name_page != 'notifies') ? '': 'Matrícula';
-								break;
-
-								case 4:
-									//$button_title_2 = ($name_page != 'notifies') ? '':'Ocorrência';
-								break;
-
-								case 5:
-									//$button_title_2 = ($name_page != 'notifies') ? '':'Trancamento';
-								break;
-
-								case 6:
-									//$button_title_2 = ($name_page != 'notifies') ? '':'Histórico';
-								break;
-									
-								default:
-									//$button_title_2 = ($name_page != 'notifies') ? '':'Outros';
-								break;
-							}
-
-							$button_title_2 = '';
 
 							$col_1 = '<a href="?id='.$row->$id.'" class="button is-link is-small"><i class="fas fa-bell"></i></a>';
 							$col_2 = isset(($row->$name_table)) ? $row->$name_table : $row->name_cou;
 							
 							$col_3 = '<a class="button is-dark is-primary is-small">'.$button_title_2.'</a>';
-							$col_4 = '<a class="button is-light is-inverted is-small" href="'.$name_page.'?id='.$row->$id.'">'.$button_title.'</a>';
+							$col_5 = '<a class="button is-light is-inverted is-small" href="'.$name_page.'?id='.$row->$id.'">'.$button_title.'</a>';
 
 							echo '
 								<tr>
@@ -63,6 +31,7 @@
 									<td>'.$col_2.'</td>
 							    	<td>'.$col_3.'</td>
 							    	<td>'.$col_4.'</td>
+							    	<td>'.$col_5.'</td>
 		    					</tr>';
 		    			}
 
