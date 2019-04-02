a15590005e75d916b3ddd612d2321ff8176efa32
Camila L <camila.leite.oliveira@gmail.com>
2019-04-02 19:55:02 -0300

    Update tables/main.php

--- load/tables/main.php
+++ load/tables/main.php
@@ -13,19 +13,49 @@
             <div class="card-table">
                 <div class="content">
                     <table class="table is-fullwidth is-striped">
+                    	<thead>
+                    		<tr>';
+                    		switch ($name_page) {
+                    			case 'classroom': 
+                    				echo '
+                    					<th>Nome</th>
+										<th>Tipo de Curso</th>';
+								break;
+								case 'courses':
+									echo '
+										<th>Nome</th>
+										<th>Tipo de Curso</th>
+										<th>Período</th>';
+								break;
+								case 'disciplines': 
+									echo '
+										<th>Nome</th>
+										<th>Nome do Curso</th>
+										<th>Professor</th>';
+								break;
+								case 'notifies':
+									echo '
+										<th>Titulo</th>
+										<th>Tipo da Notificação</th>
+										<th>Nome do Usuário</th>';
+								break;
+							}
+						echo'
+							</tr>
+						</thead>
                     	<tbody>';
 					    while($row = $query->fetch(PDO::FETCH_OBJ)){
 					    	$status_table = $Tables->Found_Item('status', $name_page);
 
 					    	switch ($row->$status_table) {
 								case 1:
-									$action_link = 'deactivate?t='.$name_page.'?id='.$row->$id;
+									$action_link = 'change?t='.$name_page.'?id='.$row->$id;
 									$action_color = 'danger';
 									$action_button = '<i class="fas fa-minus-circle"></i>';
 								break;
 
 								case 2: 
-									$action_link = 'activate?t='.$name_page.'?id='.$row->$id;
+									$action_link = 'change?t='.$name_page.'?id='.$row->$id;
 									$action_color = 'success';
 									$action_button = '<i class="fas fa-check-square">';
 								break;
@@ -37,7 +67,7 @@
 					    			$type = ($row->$type_table == 1) ? 'Ensino Médio' : 'Ensino Modular';
 					    			$col_2 = '<a class="button is-light is-inverted is-small">'.$type.'</a>';
 					    			$col_3 = '<a href="'.$name_page.'?id='.$row->$id.'" class="button is-link is-small">'.$icon.'</a>';
-									$col_4 = '<a href="'.$action_link.'" class="button is-'.$action_color.' is-small">'. $action_button.'</a>';
+									$col_4 = '<a href="'.$action_link.'" class="button is-'.$action_color.' is-small">'.$action_button.'</a>';
 									$col_5 = '';
 					    		break;
 
@@ -45,14 +75,7 @@
 					    			$col_1 = $row->$name_table;
 					    			$type = ($row->$type_table == 1) ? 'Ensino Médio' : 'Ensino Modular';
 					    			$col_2 = '<a class="button is-light is-inverted is-small">'.$type.'</a>';
-					    			$col_3 = '<a href="'.$name_page.'?id='.$row->$id.'" class="button is-link is-small">'.$icon.'</a>';
-					    			$col_4 = '<a href="'.$action_link.'" class="button is-'.$action_color.' is-small">'. $action_button.'</a>';
-					    			$col_5 = '';
-					    		break;
 
-					    		case 'disciplines':
-					    			$col_1 = $row->$name_table;
-					    			$col_2 = '<a href="#">'.$row->name_cou.'</a>';
 					    			switch ($row->period) {
 					    				case 'M':
 					    					$col_period = 'warning';
@@ -60,7 +83,7 @@
 					    				break;
 					    				
 					    				case 'T':
-					    					$col_period = 'orange';
+					    					$col_period = 'danger';
 					    					$period = 'Tarde';
 					    				break;
 
@@ -68,11 +91,24 @@
 					    					$col_period = 'link';
 					    					$period = 'Noite';
 					    				break;
+
+					    				case 'I':
+					    					$col_period = 'primary';
+					    					$period = 'Integral';
+					    				break;
 					    			}
-					    			$col_3 = '<a class="button is-'.$col_period.' is-small">'.$period.'</a>';
+
+					    			$col_3 = '<a href="schedule-grid?id='.$row->$id.'" class="button is-'.$col_period.' is-small">'.$period.'</a>';
 					    			$col_4 = '<a href="'.$name_page.'?id='.$row->$id.'" class="button is-link is-small">'.$icon.'</a>';
 					    			$col_5 = '<a href="'.$action_link.'" class="button is-'.$action_color.' is-small">'. $action_button.'</a>';
+					    		break;
 
+					    		case 'disciplines':
+					    			$col_1 = $row->$name_table;
+					    			$col_2 = '<a href="courses?n='.$row->name_cou.'">'.$row->name_cou.'</a>';
+					    			$col_3 = '<a href="courses?n='.$row->name_use.'">'.$row->name_use.'</a>';
+					    			$col_4 = '<a href="'.$name_page.'?id='.$row->$id.'" class="button is-link is-small">'.$icon.'</a>';
+					    			$col_5 = '<a href="'.$action_link.'" class="button is-'.$action_color.' is-small">'. $action_button.'</a>';
 					    		break;
 
 					    		case 'notifies':
@@ -119,6 +155,7 @@
 									$col_5 = '<a href="'.$action_link.'" class="button is-'.$action_color.' is-small">'. $action_button.'</a>';
 					    		break;
 					    	}
+
 							echo '
 								<tr>
 									<td>'.$col_1.'</td>
