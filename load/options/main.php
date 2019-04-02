10fa049dc9168fe54321d234dbfab3a119f14616
Camila L <camila.leite.oliveira@gmail.com>
2019-04-02 19:55:25 -0300

    Update options/main.php

--- load/options/main.php
+++ load/options/main.php
@@ -3,13 +3,13 @@
 	$query = $PDO->query($sql) or die ($PDO);
 	
 	echo'
-		<div class="field">
+		<div class="field" id="'.$name_page.'">
 	        <label class="label">'.$name_translated.'</label>
 	        <div class="control has-icons-left">
 	            <div class="select is-hovered is-link">
-	            	<select>';
+	            	<select name="'.$name_page.'">';
 		            	while($row = $query->fetch(PDO::FETCH_OBJ)){
-		            		echo '<option>'.$row->$name_table.'</option>';
+		            		echo '<option value="'.$row->$id_table.'">'.$row->$name_table.'</option>';
 		            	}
 	            echo '</select>
 	            </div>
