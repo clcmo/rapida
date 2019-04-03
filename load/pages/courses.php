58c4fe791a1ada6dbea7a3005589f303f3a3ec4b
Camila L <camila.leite.oliveira@gmail.com>
2019-04-03 19:18:17 -0300

    Update courses.php

--- load/pages/courses.php
+++ load/pages/courses.php
@@ -1,39 +1,9 @@
 <?php
-	$name_page = 'courses';
+	$script = $name_page = 'courses';
 
 	$name_table = $Tables->Found_Item('name', $name_page);
 	$type_table = $Tables->Found_Item('type', $name_page);
 	$id_table = $Tables->Found_Item('id', $name_page);
 
-	switch (isset($_GET['id'])) {
-		case true:
-		$script = $name_page.' WHERE '; 
-		include('load/main.php');
-		while($row = $query->fetch(PDO::FETCH_OBJ)){
-			$name_cou = $row->$name_table;
-			switch ($row->$type_table) {
-				case 1: 
-					$button_title_2 = 'Ensino Médio';
-					$checked1 = 'checked';
-					$checked2 = '';
-				break;
-				case 2: 
-					$button_title_2 = 'Ensino Modular';
-					$checked2 = 'checked';
-					$checked1 = '';
-				break;
-			}
-		}
-		break;
-		
-		case false:
-			$name_cou = 'Informe o nome do curso';
-			$checked2 = '';
-			$checked1 = '';
-			$selected_type = 'cadastrar';
-			$type_button = 'save';
-		break;
-	}
-	
-	
-	
+	$script .= ' WHERE '; 
+	include('main.php');
--- load/tables/courses.php
+++ load/tables/courses.php
@@ -3,8 +3,6 @@
 	$titulo = 'Cursos';
 	$icon = '<i class="fas fa-pencil-alt"></i>';
 
-	//$col_1 = $col_2 = $col_3 = $col_4 = $col_5 = '';
-
 	$name_table = $Tables->Found_Item('name', $name_page);
 	$id = $Tables->Found_Item('id', $name_page);
 
