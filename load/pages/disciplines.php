1ef9b0b085e7132cceaf590d74b135ee39b73f41
Camila L <camila.leite.oliveira@gmail.com>
2019-04-05 19:57:18 -0300

    Update disciplines.php

--- load/pages/disciplines.php
+++ load/pages/disciplines.php
@@ -1,9 +1,3 @@
 <?php
-	$name_page = 'disciplines';
-
-	$name_table = $Tables->Found_Item('name', $name_page);
-	$status_table = $Tables->Found_Item('status', $name_page);
-	$id_table = $Tables->Found_Item('id', $name_page);
-
-	$script = $name_page.', courses WHERE '.$name_page.'.id_cou = courses.id_cou AND ';
+	$script = $name_page = 'disciplines';
 	include('main.php');
