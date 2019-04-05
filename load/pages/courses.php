c7733d68215e6a7a481af3750178d6f83461f5ff
Camila L <camila.leite.oliveira@gmail.com>
2019-04-05 19:57:08 -0300

    Update courses.php

--- load/pages/courses.php
+++ load/pages/courses.php
@@ -1,9 +1,3 @@
 <?php
-	$script = $name_page = 'courses';
-
-	$name_table = $Tables->Found_Item('name', $name_page);
-	$type_table = $Tables->Found_Item('type', $name_page);
-	$id_table = $Tables->Found_Item('id', $name_page);
-
-	$script .= ' WHERE '; 
+	$script = $name_page = 'courses'; 
 	include('main.php');
--- load/tables/courses.php
+++ load/tables/courses.php
@@ -1,9 +1,3 @@
 <?php
-	$script = $name_page = 'courses';
-	$titulo = 'Cursos';
-	$icon = '<i class="fas fa-pencil-alt"></i>';
-
-	$name_table = $Tables->Found_Item('name', $name_page);
-	$id = $Tables->Found_Item('id', $name_page);
-
+	$name_page = 'courses';
 	include('main.php');
