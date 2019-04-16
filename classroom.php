f6495d4f354e295ce6efc99a0fca809545606995
Camila L <camila.leite.oliveira@gmail.com>
2019-04-16 20:03:30 -0300

    Update classroom.php

--- classroom.php
+++ classroom.php
@@ -1,10 +1,10 @@
 <?php
 	include('header-admin.php');
-	include('load/pages/courses.php');
+	#include('load/pages/courses.php');
 ?>
 <div class="columns">
 	<div class="column is-4">
-		<div class="tabs is-left"><?php echo $Load->MainNavegation(LINK, 'Turmas'); ?></div>
+		<div class="tabs is-left"><?php echo $Load->MainNavegation(substr(LINK, 0, (MAX-6))); ?></div>
 	</div>
 	<div class="column">
 		<div class="tabs is-right">
@@ -39,9 +39,10 @@
 </div>
 <div class="box content">
 	<!--Criar formato onde, a partir da seleção da turma, a página seja automaticamente redirecionada -->
-	<?php include('load/options/courses.php'); ?>
-	<?php
+	<?php 
+		#include('load/options/courses.php');
 		echo $Load->HeroMessage(LINK, 'Turmas', 'Visualização da Turma de '.$name_cou).'<hr/>';
-		include('load/supertables/classroom.php'); ?>
+		echo $Pages->LoadSuperTablePage(substr(LINK, 1, (MAX-6)));
+	?>
 	</div>
 <?php include('footer-admin.php'); ?>
