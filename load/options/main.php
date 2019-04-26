4d31be03b8169686d10d07f61be980c2212a0958
Camila L <camila.leite.oliveira@gmail.com>
2019-04-26 19:56:43 -0300

    Update main.php

--- load/options/main.php
+++ load/options/main.php
@@ -1,6 +1,23 @@
 <?php
-	$sql = $Tables->LoadFrom($script);
-	$query = $PDO->query($sql) or die ($PDO);
+	switch ($name_page) {
+		case 'courses':
+			$name_translated = 'Cursos';
+			$script = $name_page.' WHERE status_cou = 1';
+			$name_table = $Tables->Found_Item('name', $name_page);
+			$id_table = $Tables->Found_Item('id', $name_page);
+			$icon = '<i class="fas fa-chalkboard"></i>';
+		break;
+		
+		case 'teachers':
+			$name_translated = 'Professor';
+			$script = $name_page.', users WHERE '.$name_page.'.id_use = users.id_use AND status_use = 1';
+			$icon = '<i class="fas fa-user"></i>';
+			$name_table = $Tables->Found_Item('name', 'users');
+			$id_table = $Tables->Found_Item('id', $name_page);
+		break;
+	}
+	
+	$query = $PDO->query($Tables->SelectFrom(null, $script)) or die ($PDO);
 	
 	echo'
 		<div class="field" id="'.$name_page.'">
