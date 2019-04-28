f277512f2a2efdbeb64880e41608427b2895ea40
Camila L. Oliveira <camila.leite.oliveira@gmail.com>
2019-04-28 01:55:43 -0300

    Update teachers.php

--- teachers.php
+++ teachers.php
@@ -1,35 +1,34 @@
 <?php
-	include('header-admin.php');
-	//include('load/pages/users.php');
+	include('header.php');
 ?>
 <div class="columns">
     <div class="column is-4">
-        <div class="tabs is-left"><?php echo $Load->MainNavegation(); ?></div>
+        <div class="tabs is-left"><?php echo $Navegation->MainNavegation($link); ?></div>
     </div>
     <div class="column">
     	<div class="tabs is-centered">
 		  	<ul>
 		    	<li class="is-active">
-		      		<a>
-		        		<span class="icon is-small"><i class="fas fa-chalkboard" aria-hidden="true"></i></span>
+		      		<a href="#">
+		        		<span class="icon is-small"><i class="fas fa-user" aria-hidden="true"></i></span>
 		        		<span>Todos</span>
 		      		</a>
 		    	</li>
 		    	<li>
-		      		<a>
-		        		<span class="icon is-small"><i class="fas fa-chalkboard-teacher" aria-hidden="true"></i></span>
+		      		<a href="#">
+		        		<span class="icon is-small"><i class="fas fa-user" aria-hidden="true"></i></span>
 		        		<span class="content is-link">Ativos</span>
 		      		</a>
 		    	</li>
 		    	<li>
-		      		<a>
-		        		<span class="icon is-small"><i class="fas fa-chalkboard" aria-hidden="true"></i></span>
+		      		<a href="#">
+		        		<span class="icon is-small"><i class="fas fa-user" aria-hidden="true"></i></span>
 		        		<span class="content is-danger">Inativos</span>
 		      		</a>
 		    	</li>
 		  		<li>
-		      		<a>
-		        		<span class="icon is-small"><i class="fas fa-chalkboard-teacher" aria-hidden="true"></i></span>
+		      		<a href="new-user">
+		        		<span class="icon is-small"><i class="fas fa-user" aria-hidden="true"></i></span>
 		        		<span class="content is-blak">Add Professor</span>
 		      		</a>
 		    	</li>
@@ -38,9 +37,8 @@
     </div>
 </div>
 <div class="box content">
-	<?php
-		echo $Load->HeroMessage(LINK, ucfirst('professores'), 'Visualização dos Professores Cadastrados'); ?>
+	<?php echo $Navegation->HeroMessage(ucfirst('professores'), 'Visualização dos Professores Cadastrados'); ?>
 	<hr/>
-	<?php include('load/articles/teachers.php'); ?>
+	<?php echo $Pages->LoadArticlePage($link); ?>
 </div>
 <?php include('footer-admin.php'); ?>
