61f6a6433521e20af6f4c3010b29c5b9023486e4
Camila L. Oliveira <camila.leite.oliveira@gmail.com>
2019-04-28 01:54:13 -0300

    Update employees.php

--- employees.php
+++ employees.php
@@ -1,34 +1,34 @@
 <?php
-	include('header-admin.php');
+	include('header.php');
 	//include('load/pages/users.php');
 ?>
 <div class="columns">
     <div class="column is-4">
-        <div class="tabs is-left"><?php echo $Load->MainNavegation(); ?></div>
+        <div class="tabs is-left"><?php echo $Navegation->MainNavegation($link); ?></div>
     </div>
     <div class="column">
     	<div class="tabs is-centered">
 		  	<ul>
 		    	<li class="is-active">
-		      		<a>
+		      		<a href="#">
 		        		<span class="icon is-small"><i class="fas fa-user" aria-hidden="true"></i></span>
 		        		<span>Todos</span>
 		      		</a>
 		    	</li>
 		    	<li>
-		      		<a>
+		      		<a href="#">
 		        		<span class="icon is-small"><i class="fas fa-user" aria-hidden="true"></i></span>
 		        		<span class="content is-link">Ativos</span>
 		      		</a>
 		    	</li>
 		    	<li>
-		      		<a>
+		      		<a href="#">
 		        		<span class="icon is-small"><i class="fas fa-user" aria-hidden="true"></i></span>
 		        		<span class="content is-danger">Inativos</span>
 		      		</a>
 		    	</li>
 		  		<li>
-		      		<a>
+		      		<a href="new-user">
 		        		<span class="icon is-small"><i class="fas fa-user" aria-hidden="true"></i></span>
 		        		<span class="content is-blak">Add Funcionário</span>
 		      		</a>
@@ -40,6 +40,6 @@
 <div class="box content">
 	<?php echo $Navegation->HeroMessage(ucfirst('funcionários'), 'Visualização dos Funcionários Cadastrados'); ?>
 	<hr/>
-	<?php #include('load/articles/employees.php'); ?>
+	<?php echo $Pages->LoadArticlePage($link); ?>
 </div>
 <?php include('footer-admin.php'); ?>
