81ef4f920fd4b63d5027290ac9cc401c46f82d10
Camila L. Oliveira <camila.leite.oliveira@gmail.com>
2019-04-28 01:54:34 -0300

    Update main.php

--- functions/main.php
+++ functions/main.php
@@ -26,21 +26,18 @@
     # 4 - Definição de Conexão
     $PDO = $Load->DataBase();
     define('LINK',       $_SERVER['REQUEST_URI']);
+    $link = $Load->DiscoverLink();
     define('MAIN_EMAIL', 'someone@somewhere.com');
     $error = array();
 
-    # 5 - Definições de Paginação
-    $vf = 10;
-    $pg = isset($_GET['pg']) ? $_GET['pg'] : '';
-    $pc = (!$pg) ? 1 : $pg;
-    $vi = $pc - 1;
-    $vi = $vi * $vf;
-    define('MAX', count(LINK));
-
-    # 6 - Definições de Inserção/Edição
+    # 5 - Definições de Inserção/Edição
     $id = (isset($_SESSION['id'])) ? $_SESSION['id'] : (isset($_GET['id'])) ? $_GET['id'] : '';
     $email = isset($_GET['email']) ? $_GET['email'] : '';
     $placeholder = isset($_GET['email']) ? $_GET['email'] : 'Informe seu email';
+    $picture = isset($_GET['email']) ? $Load->Gravatar($_GET['email']) : $Load->Gravatar();
+    $password = 'Informe sua senha';
+    $password_conf = 'Repita corretamente';
+
     switch ($id) {
         case true:
             $selected_type = 'editar';
@@ -52,3 +49,20 @@
             $type_button = 'save';
         break;
     }
+
+    # 6 - Definições de Paginação
+    $vf = 10;
+    $pg = isset($_GET['pg']) ? $_GET['pg'] : '';
+    $pc = (!$pg) ? 1 : $pg;
+    $vi = $pc - 1;
+    $vi = $vi * $vf;
+   
+    define('MAX', strlen($link));
+    $sizeof = array();
+    $sizeof[1] = MAX;
+    #echo $sizeof[1].' ';
+    $sizeof[2] = strlen(strstr(LINK, '?id='.$id));
+    #echo $sizeof[2];
+    $link = (isset($_GET['id'])) ? $Load->DiscoverLink($link, ($sizeof[1] - $sizeof[2])) : $Load->DiscoverLink();
+
+   
