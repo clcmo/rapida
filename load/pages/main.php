41f628183fe56d63a8469d9bf943ab5d2bd0036e
Camila L. Oliveira <camila.leite.oliveira@gmail.com>
2019-04-29 14:10:58 -0300

    Update main.php

--- load/pages/main.php
+++ load/pages/main.php
@@ -1,11 +1,7 @@
 <?php
-	#ouxar tabela histórico
-	#$id = (isset($_GET['id'])) ? $_GET['id'] : (isset($_SESSION['id'])) ? $_SESSION['id'] : '';
-	#puxa a id informada
-
 	#com o nome da página estaremos puxando:
-	$script = $link;
-	#script sql
+	$script = $link; #script sql
+
 	$type_table = $Tables->Found_Item('type', $link);
 	#o tipo na tabela informada
 	$status_table = $Tables->Found_Item('status', $link);
@@ -14,16 +10,28 @@
 	#o titulo/nome na tabela informada 
 	$id_table = $Tables->Found_Item('id', $link);
 	#o id na tabela informada
+	
 	switch ($link) {
+		case 'profile': 
+			$id = isset(($_GET['id'])) ? $_GET['id'] : $_SESSION['id'];
+			$script = 'users WHERE ';
+			$id_table = $Tables->Found_Item('id', 'users');
+		break;
+
+		case 'historic':
+		break;
+		
 		case 'courses':
 		#case 'users': $script .= ' WHERE '; break;
+		break;
+		
 		case 'new-user':
 			$name_use = '';
 			$year = date('Y', strtotime(TODAY));
 			$signup_date = date('d/m/Y', strtotime(TODAY));
 			$email = isset($_GET['email']) ? $_GET['email'] : '';
 			$login = '';
-			$photo = $Load->Gravatar($email);
+			$photo = $Load->Gravatar(MAIN_EMAIL);
 			$cep = $address = $number = $neighborhood = $city = $state = $rg = $cpf = $phone = '';
 			$birthday_date = date('d/m/Y', strtotime(TODAY));
 			$birthday_year = date('Y', strtotime(TODAY));
@@ -32,38 +40,197 @@
 			$string = 'Tipo de Usuário';
 			#criar radio button
 		break;
+		
 		case 'disciplines': $script .= ', courses WHERE '.$link.'.id_cou = courses.id_cou AND '; break;
+		
 		case 'notifies':
 			$con = $PDO->query($Tables->SelectFrom('name_use, type_use','users WHERE id_use LIKE '.$_SESSION['id'].' AND status_use = 1')) or die ($PDO);
 			while($row = $con->fetch(PDO::FETCH_OBJ)){
 				$name_use = $row->name_use;
 				switch ($row->type_use){
-					case 1:
-						#diretor
-					break;
+					case 2: break;
+					case 3: $script .=' AND users.id_use = '.$_SESSION['id']; $button_title = 'Gerar Documento'; break;
+					case 4: $script .= ', users WHERE users.id_use = '.$name_page.'.id_use AND '; break;
+					case 5: $script .= ', users WHERE users.id_use = '.$name_page.'.id_use AND users.id_use = '.$_SESSION['id'].' AND '; break;
+					default: break;
+				}
+			}
+		break;
 
-					case 2:
-						#coordenador
-					break;
+		case 'schedule-grid':
+		break;
+	}
 
-					case 3:
-						#funcionário
-						$script .= ', users WHERE users.id_use = '.$name_page.'.id_use AND ';
-						$button_title = 'Gerar Documento';
-					break;
-							
-					case 4:
-						#professor
-					break;
+	switch ($id) {
+		case false:
+			# code...
+		break;
+		
+		default:
+			$script.= $id_table.' = '.$id;
+			$PDO = $Load->DataBase();
+			$con = $PDO->query($Tables->SelectFrom(null, $script)) or die ($PDO);
+			$cont = $Tables->CountViewTable(null, $script);
+			while($row = $con->fetch(PDO::FETCH_OBJ)){
+				switch($link){
+					case 'profile':
+						$name_use = $row->name_use;
+						$year = ($row->signup_date) ? date('Y', strtotime($row->signup_date)) : '';
+						$signup_date = ($row->signup_date) ? date('d/m/Y', strtotime($row->signup_date)) : '';
+						$email = ($row->email) ? $row->email : '';
+						$login = ($row->login) ? $row->login : '';
+						$photo = ($row->photo) ? SERVER.'uploads/'.$row->photo : $Load->Gravatar($email);
+						$cep = ($row->cep) ? $row->cep : '';
+						$address = ($row->address) ? $row->address : '';
+						$number = ($row->number) ? $row->number : '';
+						$neighborhood = ($row->neighborhood) ? $row->neighborhood : '';
+						$city = ($row->city) ? $row->city : '';
+						$state = ($row->state) ? $row->state : '';
+						$rg = ($row->rg) ? $row->rg : '';
+						$cpf = ($row->cpf) ? $row->cpf : '';
+						$phone = ($row->phone) ? $row->phone : '';
+						$birthday_date = ($row->birthday_date) ? date('d/m/Y', strtotime($row->birthday_date)) : '';
+						$birthday_year = ($row->birthday_date) ? date('Y', strtotime($row->birthday_date)) : '0';
 
-					case 5:
-						$script .= ', users WHERE users.id_use = '.$name_page.'.id_use AND users.id_use = '.$_SESSION['id'].' AND ';
-						/*$profile_link = SERVER.'profile';*/
+						$data = $name_par = $phone_par = $rg_par = $cpf_par = $area = '';
+
+						switch ($row->type_use) {
+							case 1:	
+								$type = 'Diretor';
+								$table = 'director';
+								/*$string = 'Area';
+								$query = '';
+								$input = '';*/
+							break;
+							case 2: 
+								$type = 'Coordenador';
+								$string = 'Curso';
+								$table = 'coordenators';
+								/*$query = '';
+								$input = '';*/
+							break;
+							case 3:
+								$type = 'Funcionário';
+								$table = 'employees';
+								$string = 'Area';
+								$found_area = $Tables->Found_Item('area', $table);
+								$query = $PDO->query($Tables->SelectFrom($found_area, $table.', users WHERE users.id_use LIKE '.$id.' AND users.status_use = 1 AND '.$table.'.id_use = users.id_use')) or die ($PDO);
+								while ($row = $query->fetch(PDO::FETCH_OBJ)) { $area = ($row->$found_area) ? $row->$found_area : ''; }
+								$input = '
+									<div class="control has-icons-left has-icons-right">
+					  					<input class="input is-link" type="text" placeholder="'.$area.'" value="'.$area.'" name="area">
+					  					<span class="icon is-small is-left"><i class="fas fa-user"></i></span>
+										<span class="icon is-small is-right"><i class="fas fa-check"></i></span>
+									</div>';
+							break;
+							case 4: 
+								$type = 'Professor';
+								$table = 'teachers';
+								$string = 'Area';
+								$found_area = $Tables->Found_Item('area', $table);
+								$query = $PDO->query($Tables->SelectFrom($found_area, $table.', users WHERE users.id_use LIKE '.$id.' AND users.status_use = 1 AND '.$table.'.id_use = users.id_use')) or die ($PDO);
+								while ($row = $query->fetch(PDO::FETCH_OBJ)) { $area = ($row->$found_area) ? $row->$found_area : ''; }
+								$input = '
+									<div class="control has-icons-left has-icons-right">
+					  					<input class="input is-link" type="text" placeholder="'.$area.'" value="'.$area.'" name="area">
+					  					<span class="icon is-small is-left"><i class="fas fa-user"></i></span>
+										<span class="icon is-small is-right"><i class="fas fa-check"></i></span>
+									</div>';
+							break;
+							case 5: 
+								$type = 'Aluno';
+								$string = 'Turma';
+								$table = 'students';
+								#$found_area = $Tables->Found_Item('area', $table);
+								$query = $PDO->query($Tables->SelectFrom('name_cou', $table.', users, classroom, courses WHERE users.id_use LIKE '.$id.' AND users.status_use = 1 AND courses.id_cou = classroom.id_cla AND classroom.id_cla = '.$table.'.id_cla AND users.id_use = '.$table.'.id_use')) or die ($PDO);
+								while ($row = $query->fetch(PDO::FETCH_OBJ)) { $area = ($row->name_cou) ? $row->name_cou : ''; }
+								$input = '
+									<div class="control has-icons-left has-icons-right">
+					  					<input class="input is-link" type="text" placeholder="'.$area.'" value="'.$area.'" name="area" disabled>
+					  					<span class="icon is-small is-left"><i class="fas fa-user"></i></span>
+										<span class="icon is-small is-right"><i class="fas fa-check"></i></span>
+									</div>';
+
+								if($birthday_year - YEAR <= 18){
+									#parents query
+									$query = $PDO->query($Tables->SelectFrom(null, $table.', parents, users WHERE parents.id_stu = '.$table.'.id_stu AND '.$table.'.id_use = users.id_use AND users.id_use LIKE '.$id)) or die($PDO);
+									while ($row = $query->fetch(PDO::FETCH_OBJ)) {
+										$name_par = isset($row->name_par) ? $row->name_par : '';
+										$cpf_par = isset($row->cpf_par) ? $row->cpf_par : '';
+										$rg_par = isset($row->rg_par) ? $row->rg_par : '';
+										$phone_par = isset($row->phone_par) ? $row->phone_par : '';
+									}
+									$data = '
+										<p class="title is-small">Informações de Contato</p>
+						  				<div class="columns">
+											<div class="column is-6">
+								  			<div class="field">
+								  				<label class="label">Pai / Mãe / Responsável:</label>
+								  				<div class="control has-icons-left has-icons-right">
+						  							<input class="input is-link" type="text" placeholder="'.$name_par.'" value="'.$name_par.'" name="name_par" disabled>
+						  							<span class="icon is-small is-left"><i class="fas fa-user"></i></span>
+													<span class="icon is-small is-right"><i class="fas fa-check"></i></span>
+												</div>
+								  			</div>
+								  			</div>
+								  			<div class="column is-6">
+								  			<div class="field">
+								  				<label class="label">Telefone de Contato:</label>
+								  				<div class="control has-icons-left has-icons-right">
+						  							<input class="input is-link" type="text" placeholder="'.$phone_par.'" value="'.$phone_par.'" name="phone_par" disabled>
+						  							<span class="icon is-small is-left"><i class="fas fa-user"></i></span>
+													<span class="icon is-small is-right"><i class="fas fa-check"></i></span>
+												</div>
+								  			</div>
+								  			</div>
+								  		</div>
+								  		<div class="columns">
+								  			<div class="column is-6">
+								  			<div class="field">
+								  				<label class="label">RG:</label>
+								  				<div class="control has-icons-left has-icons-right">
+						  							<input class="input is-link" type="text" placeholder="'.$rg_par.'" value="'.$rg_par.'" name="rg_par" disabled>
+						  							<span class="icon is-small is-left"><i class="fas fa-user"></i></span>
+													<span class="icon is-small is-right"><i class="fas fa-check"></i></span>
+												</div>
+								  			</div>
+								  			</div>
+								  			<div class="column is-6">
+								  			<div class="field">
+								  				<label class="label">CPF:</label>
+								  				<div class="control has-icons-left has-icons-right">
+						  							<input class="input is-link" type="text" placeholder="'.$cpf_par.'" value="'.$cpf_par.'" name="cpf_par" disabled>
+						  							<span class="icon is-small is-left"><i class="fas fa-user"></i></span>
+													<span class="icon is-small is-right"><i class="fas fa-check"></i></span>
+												</div>
+								  			</div>
+								  			</div>
+								  		</div>';
+								}
+							break;
+							default:
+								$string = 'Area';
+								$found_area = $Tables->Found_Item('area', $table);
+								$query = $PDO->query($Tables->SelectFrom($found_area, 'users, '.$table.' WHERE users.id_use LIKE '.$id.' AND users.status_use = 1 AND '.$table.'.id_use = users.id_use')) or die ($PDO);
+								while ($row = $query->fetch(PDO::FETCH_OBJ)) { $area = ($row->$found_area) ? $row->$found_area : ''; }
+								$input = '
+									<div class="control has-icons-left has-icons-right">
+					  					<input class="input is-link" type="text" placeholder="'.$area.'" value="'.$area.'" name="area">
+					  					<span class="icon is-small is-left"><i class="fas fa-user"></i></span>
+										<span class="icon is-small is-right"><i class="fas fa-check"></i></span>
+									</div>';
+							break;
+						}
 					break;
 				}
 			}
 		break;
 	}
+
+
+
+
+/*
 
 	switch($id){
 		case false:
@@ -90,12 +257,11 @@
 		break;
 
 		case true:
-			$script.= $id_table.' = '.$id;
-			$PDO = $Load->DataBase();
+			
+			
 			#echo $Tables->SelectFrom(null, $script);
-			$con = $PDO->query($Tables->SelectFrom(null, $script)) or die ($PDO);
-			$cont = $Tables->CountViewTable(null, $script);
-			while($row = $con->fetch(PDO::FETCH_OBJ)){
+			
+			
 				switch ($link) {
 					case 'courses':
 						$name_cou = $row->$name_table;
@@ -146,3 +312,4 @@
 	}
 	
 	#$placeholder = $email = isset($_GET['email']) ? $_GET['email'] : '';
+*/
