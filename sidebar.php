9874194083c4dad48d8bf7814367a875e4ee6a70
Camila L. Oliveira <camila.leite.oliveira@gmail.com>
2019-04-30 00:43:36 -0300

    Update gitignore

--- sidebar.php
+++
@@ -1,5 +0,0 @@
-<section class="section">    
-    <div class="container">
-        <!--<div class="columns">
-            <div class="column is-2"><?php echo $User->ShowSidebarMenu($_SESSION['id']); ?></div>
-            <div class="column is-10">-->
