ab868b0003f1373933ae9d6442433ac8e17b694f
Camila L <camila.leite.oliveira@gmail.com>
2019-05-14 19:59:10 -0300

    Update ops.php

--- functions/ops.php
+++ functions/ops.php
@@ -1,6 +1,6 @@
 <div class="column is-4 is-offset-4">
 	<div class="box">
-		<h3 class="title is-medium"><?php echo $title; ?></h3>
+		<h3 class="title is-medium has-text-centered"><?php echo $title; ?></h3>
 		<p class="content"><?php echo $message; ?></p>
 		<p class="links">
 			<a href="<?php echo $links[1]; ?>"><?php echo $links[2]; ?></a> &nbsp;·&nbsp;
