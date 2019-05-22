f793c5ec2838b6fa4c2f2c49d855d3c874aa9426
Camila L <camila.leite.oliveira@gmail.com>
2019-05-22 13:10:49 -0300

    Update schedule-grid.php

--- schedule-grid.php
+++ schedule-grid.php
@@ -1,39 +1,4 @@
 <?php
 	include('header.php');
-	$con = $PDO->query($Tables->SelectFrom('name_cou', 'courses WHERE id_cou = '.$_GET['id'])) or die ($PDO);
-	while($row = $con->fetch(PDO::FETCH_OBJ)){
-		$name_cou = $row->name_cou;
-	}
-?>
-<div class="columns">
-    <div class="column">
-        <div class="tabs is-left"><?php echo $Navegation->MainNavegation($link); ?></div>
-    </div>
-    <!--<div class="column">
-		<div class="tabs is-right">
-			<ul>
-				<li class="is-active">
-					<a href="message">
-						<span class="icon is-small"><i class="fas fa-chalkboard-teacher" aria-hidden="true"></i></span>
-			    		<span class="content is-blak">Solicitar Reserva de Sala</span>
-			    	</a>
-			    </li>
-			    <li>
-					<a>
-						<span class="icon is-small"><i class="fas fa-chalkboard-teacher" aria-hidden="true"></i></span>
-			    		<span class="content is-blak">Grade de Manutenção</span>
-			    	</a>
-			    </li>
-			</ul>
-		</div>
-	</div>-->
-</div>
-<div class="box content">
-	<!--Criar formato onde, a partir da seleção do curso, a página seja automaticamente redirecionada -->
-	<?php 
-		#include('load/options/courses.php'); 
-		echo $Navegation->HeroMessage('Grade de Horários', 'Visualização da grade de horários para '.$name_cou); ?>
-	<hr />
-<?php include('load/pages/'.$link.'.php'); ?>
-</div>
-<?php include('footer.php'); ?>
+	include('functions/pages/main.php');
+	include('footer.php');
