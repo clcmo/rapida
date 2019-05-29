007b5556683cecbc0a86246719cb613d701056a6
Camila L <camila.leite.oliveira@gmail.com>
2019-05-29 19:40:16 -0300

    Create about.php

---
+++ models/about.php
@@ -0,0 +1,9 @@
+<div class="column">
+	<div class="box">
+		<h3 class="title is-medium has-text-centered"><?php echo $title; ?></h3>
+		<p class="content has-text-left"><?php echo $message; ?></p>
+		<p class="links">
+			<a href="help">Ajuda</a>
+		</p>
+	</div>
+</div>
