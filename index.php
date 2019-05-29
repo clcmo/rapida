0aba1b197a9296e782e2b0dd6b1db19a10a8146a
Camila L <camila.leite.oliveira@gmail.com>
2019-05-29 19:40:04 -0300

    Update index.php

--- index.php
+++ index.php
@@ -54,13 +54,13 @@
             <div class="hero-body">
                 <div class="container has-text-centered">
                     <div class="column is-6 is-offset-3">
-                        <h1 class="title is-logo is-large">Estamos Chegando</h1>
-                        <h2 class="subtitle">A Rápida está em desenvolvimento. Para saber planos e pacotes, insira seu e-mail e saiba antes de todo mundo.</h2>
+                        <h1 class="title is-logo is-large">bem vindos</h1>
+                        <h2 class="subtitle">você já pode entrar na Rápida. Basta informar seu e-mail abaixo: </h2>
                         <form action="" method="post">
                             <div class="box">
                                 <div class="field is-grouped">
-                                    <p class="control is-expanded"><input class="input" type="text" placeholder="Insira seu e-mail"></p>
-                                    <p class="control"><input class="button is-info" type="submit" name="send" value="Me Informe" /></p>
+                                    <p class="control is-expanded"><input class="input" type="email" name="email" placeholder="Insira seu e-mail"></p>
+                                    <p class="control"><input class="button is-info" type="submit" name="send" value="Entrar" /></p>
                                 </div>
                             </div>
                         </form>
@@ -68,5 +68,12 @@
                 </div>
             </div><?php
         break;
-    } 
+    }
+
+    if(isset($_POST['send'])) {
+        # Resgata variáveis do formulário
+        $email = isset($_POST['email']) ? $_POST['email'] : '';
+        # Redireciona para a página de Login
+        $Load->Link('login');                 
+    }
     include('footer.php');
