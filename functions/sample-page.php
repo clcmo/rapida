a0df20c4fc76bef13eeb578e7a316a359db34790
Camila L <camila.leite.oliveira@gmail.com>
2019-05-29 19:39:53 -0300

    Update sample-page.php

--- functions/sample-page.php
+++ functions/sample-page.php
@@ -5,43 +5,6 @@
 		<?php
 			switch($link){
 				case SERVER: case 'change': case 'documents': case 'ops': break;
-<<<<<<< HEAD
-				default: 
-					?>
-					<div class="column">
-						<div class="tabs is-right">
-						  	<ul>
-						    	<li class="is-active">
-						      		<a href="">
-						        		<span class="icon is-small"><i class="fas fa-chalkboard" aria-hidden="true"></i></span>
-						        		<span>Todos</span>
-						        	</a>
-						        </li>
-						        <li>
-						        	<a href="?type=1">
-						        		<span class="icon is-small"><i class="fas fa-chalkboard-teacher" aria-hidden="true"></i></span>
-						        		<span class="content is-link">Ativos</span>
-						    		</a>
-						    	</li>
-						    	<li>
-						    		<a href="?type=2">
-						    			<span class="icon is-small"><i class="fas fa-chalkboard" aria-hidden="true"></i></span>
-						    			<span class="content is-danger">Inativos</span>
-						    		</a>
-						    	</li>
-						    	<!--<li>
-						    		<a>
-						    			<span class="icon is-small"><i class="fas fa-chalkboard-teacher" aria-hidden="true"></i></span>
-						    			<span class="content is-blak">Add Turma</span>
-						    		</a>
-						    	</li>-->
-							</ul>
-						</div>
-					</div>
-					<?php
-				break;
-			}
-=======
 				case 'reserve': case 'schedule-grid':
 				?>
 				<div class="column">
@@ -105,18 +68,10 @@
 		<?php
 			break;
 		}
->>>>>>> origin/master
 		?>
 	</div>
 	<div class="box content">
 		<?php
-<<<<<<< HEAD
-			#Criar formato onde, a partir da seleção da turma, a página seja automaticamente redirecionada
-			#include('load/options/courses.php');
-			#echo $link;
-			#echo $Navegation->HeroMessage('Turmas', 'Visualização da Turma de '.$name_cou).'<hr/>';
-			#echo $Pages->LoadSuperTablePage($link);
-=======
 			switch($link){
 				case 'change': case 'ops': break;
 				case 'classroom': case 'historic': case 'schedule-grid': case 'reserve': 
@@ -133,6 +88,5 @@
 			switch($link){
 				case 'change': echo $res; break;
 			}
->>>>>>> origin/master
 		?>
 	</p>
